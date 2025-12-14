5b5b06a45782e441e646d2e560c10df0c3df5b20
fatbob01 <fatbob01@hotmail.com>
2025-12-14 08:02:03 -0500

    Build mount-root-relative symlink targets

---
+++ backend.Tests/DatabaseStoreSymlinkFileTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using NzbWebDAV.Database.Models;
+using NzbWebDAV.WebDav;
+using Xunit;
+
+public class DatabaseStoreSymlinkFileTests
+{
+    [Fact]
+    public void GetTargetPath_UsesMountRootAndContentLayout()
+    {
+        var mountDir = "X:/nzbdav";
+        var davItem = new DavItem
+        {
+            Id = Guid.NewGuid(),
+            Path = "/completed-symlinks/movies/Inception.mkv",
+            Name = "Inception.mkv",
+            ParentId = DavItem.SymlinkFolder.Id,
+            Type = DavItem.ItemType.NzbFile,
+            IdPrefix = "abcde",
+            CreatedAt = DateTime.UtcNow,
+        };
+
+        var target = DatabaseStoreSymlinkFile.GetTargetPath(davItem, mountDir);
+
+        Assert.Equal("/nzbdav/content/movies/Inception.mkv", target);
+        Assert.Equal(target, Path.GetFullPath(target, "/srv/media/movies"));
+    }
+}
